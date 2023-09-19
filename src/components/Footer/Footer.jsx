b824b0c51c45bffb2e9c4a600c599cd93eed3469
Adro82 <amendi82@gmail.com>
2023-09-19 16:11:24 +0200

    done

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,5 @@
+import './Footer.css'
+
+const Footer = () => <footer> Todos los derechos reservados a Loradro</footer>
+
+export default Footer
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,28 @@
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+const Navigation = () => {
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Link to="/">
+                            <Nav.Link as="span">Inicio</Nav.Link>
+                        </Link>
+                        <Link to="/galeria">
+                            <Nav.Link as="span">Comidas</Nav.Link>
+                        </Link>
+                        <Link to="/crear">
+                            <Nav.Link as="span">Crear comida</Nav.Link>
+                        </Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Navigation
